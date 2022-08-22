aa6e2517ea3f6be5854fd10d55fea5590a172a48
Anandhmahen <anandhkumar887@gmail.com>
2022-08-22 15:09:15 +0530

    removed unwanted comments from main.tf

--- main.tf
+++ main.tf
@@ -30,21 +30,13 @@ resource "aws_api_gateway_integration" "USER_GET_INTEGRATION" {
     integration_http_method = "GET"
     type = "HTTP"
     uri = " https://isro.vercel.app/api/centres"
-    # request_templates = {
-    #     "application/json" = ""
-    # }
-    # passthrough_behavior = "WHEN_NO_MATCH"
 }
 
 resource "aws_api_gateway_method_response" "USER_GET_METHOD_RESPONSE" {
     rest_api_id = aws_api_gateway_rest_api.EXAMPLE_API_GATEWAY.id
     resource_id = aws_api_gateway_resource.USER_RESOURCE.id
     http_method = aws_api_gateway_method.USER_GET_METHOD.http_method
-    # http_method = "GET"
     status_code = "200"
-    # response_models = {
-    #     "application/json" = "Empty"
-    # }
 }
 
 resource "aws_api_gateway_integration_response" "USER_GET_INTEGRATION_RESPONSE" {
@@ -52,11 +44,7 @@ resource "aws_api_gateway_integration_response" "USER_GET_INTEGRATION_RESPONSE" 
     resource_id = aws_api_gateway_resource.USER_RESOURCE.id
     http_method = aws_api_gateway_method.USER_GET_METHOD.http_method
     status_code = aws_api_gateway_method_response.USER_GET_METHOD_RESPONSE.status_code
-    # response_templates = {
-    #     "application/json" = ""
-    # }
 }
-
 
 resource "aws_api_gateway_deployment" "USER_DEPLOYMENT" {
     rest_api_id = aws_api_gateway_rest_api.EXAMPLE_API_GATEWAY.id
